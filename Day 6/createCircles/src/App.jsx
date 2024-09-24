fffacaf7be0a2a90538c1c03645d678723d09048
Joy Bose <clevercoderjoy@gmail.com>
2024-09-24 17:19:26 +0530

    circles

--- Day 6/createCircles/src/App.jsx
+++ Day 6/createCircles/src/App.jsx
@@ -2,39 +2,64 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 function App() {
-  const [mousePosition, setMousePosition] = useState({ mouseX: 0, mouseY: 0 });
-  const [circleData, setCircleData] = useState({ circles: [], undo: [], redo: [] });
+  const [circles, setCircles] = useState([]);
+  const [undo, setUndo] = useState([]);
+  const [redo, setRedo] = useState([]);
 
 
   const generateRandomColors = () => {
-    return colors = Math.floor(Math.random() * (256 - 0) + 0);
+    const colors = Math.floor(Math.random() * (256 - 0) + 0);
+    return colors;
   }
   const handleWindowClick = (e) => {
-    setMousePosition({ mouseX: `${e.clientY - 23}px`, mouseY: `${e.clientX - 10}px` })
+    const newColor = `rgb(${generateRandomColors()} ${generateRandomColors()} ${generateRandomColors()})`;
+    const newCircle = (<div className="circle" style={{ top: `${e.clientY - 100}px`, left: `${e.clientX - 15}px`, backgroundColor: newColor }} />)
+    setCircles(prev => [...prev, newCircle])
   }
+  const handleUndoClick = (e) => {
+    e.stopPropagation();
+    const removedCircle = circles.pop();
+    setUndo(prev => [...prev, removedCircle]);
+  };
+
+  const handleResetClick = (e) => {
+    e.stopPropagation();
+    setCircles([]);
+    setUndo([]);
+    setRedo([]);
+  }
+
+  const handleRedoClick = (e) => { };
+
+  console.log("circles", circles);
+  console.log("undo", undo);
+  console.log("redo", redo);
+
 
   useEffect(() => {
     window.addEventListener("click", handleWindowClick);
-    console.log(mousePosition)
 
     return () => {
       window.removeEventListener("click", handleWindowClick)
     }
-  }, [mousePosition]);
+  }, []);
 
   return (
     <>
       <div className="buttonContainer">
-        <button className="btn">
+        <button className="btn" onClick={handleUndoClick}>
           Undo
         </button>
-        <button className="btn">
+        <button className="btn" disabled={circles.length <= 0 ? true : false} onClick={handleResetClick} style={{ cursor: (circles.length <= 0 ? "not-allowed" : "pointer") }}>
           Reset
         </button>
         <button className="btn">
           Redo
         </button>
       </div>
+      {
+        circles?.map((circle, index) => <div key={index}>{circle}</div>)
+      }
     </>
   )
 }
