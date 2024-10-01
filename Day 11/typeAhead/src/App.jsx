b1b296a3bdaa80d1d1f46f365ad3355148826068
Joy Bose <clevercoderjoy@gmail.com>
2024-10-01 11:03:42 +0530

    typeahead done almost

--- Day 11/typeAhead/src/App.jsx
+++ Day 11/typeAhead/src/App.jsx
@@ -2,27 +2,54 @@ import { useState } from "react";
 
 function App() {
 
-  const suggestions = [
-    { name: "ybl" },
-    { name: "oksbi" },
-    { name: "okhdfc" },
-    { name: "upi" },
-    { name: "okaxis" },
-    { name: "okicici" },
-    { name: "paytm" },
-    { name: "googlepay" },
-    { name: "phonepe" },
-  ];
+  const suggestions = ["ybl", "oksbi", "okhdfc", "upi", "okaxis", "okicici", "paytm", "googlepay", "phonepe"];
+
 
   const [filteredSuggestions, setFilteredSuggestion] = useState(suggestions);
   const [showSuggestions, setShowSuggestions] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(0);
   const [inputValue, setInputValue] = useState("");
-  const [activeIndex, setActiveIndex] = useState(-1);
 
   const handleInputChange = (e) => {
     const value = e.target.value;
     setInputValue(value);
-    setShowSuggestions(value.includes("@"));
+    if (value.length > 1) {
+      setShowSuggestions(value.includes("@"));
+    }
+    handleSuggestions(value);
+  }
+
+  const handleSuggestions = (value) => {
+    const [val, slug] = value.split("@");
+    const filteredUpis = suggestions.filter((upi) => upi.includes(slug));
+    setFilteredSuggestion(filteredUpis);
+  }
+
+  const handleSelectedSuggestion = (suggestion) => {
+    const [beforeAt] = inputValue.split("@");
+    setInputValue(beforeAt + "@" + suggestion);
+    setShowSuggestions(false);
+  }
+
+  const handleKeyDown = (e) => {
+    if (inputValue.includes("@")) {
+      if (e.key === "ArrowDown") {
+        setActiveIndex((prevIndex) => (prevIndex + 1) % filteredSuggestions.length);
+      }
+      if (e.key === "ArrowUp") {
+        setActiveIndex((prevIndex) => (prevIndex - 1 + filteredSuggestions.length) % filteredSuggestions.length);
+      }
+      if (e.key === "Enter") {
+        const selectedSuggestion = filteredSuggestions[activeIndex];
+        if (selectedSuggestion) {
+          handleSelectedSuggestion(selectedSuggestion);
+        }
+      }
+    }
+  };
+
+  const handleMouseEnter = (index) => {
+    setActiveIndex(index);
   }
 
   return (
@@ -35,13 +62,14 @@ function App() {
             autoFocus
             className="input-field"
             onChange={handleInputChange}
+            onKeyDown={handleKeyDown}
             value={inputValue}
           />
           {
             showSuggestions && (
               <div className={`suggestions-box`}>
                 {
-                  filteredSuggestions.map((data, index) => <div className="suggestion-item" key={index}>{data.name}</div>)
+                  filteredSuggestions.map((suggestion, index) => <div className={`suggestion-item ${index === activeIndex ? "active" : ""}`} onClick={() => handleSelectedSuggestion(suggestion)} onMouseEnter={() => handleMouseEnter(index)} key={index}>{suggestion}</div>)
                 }
               </div>
             )
