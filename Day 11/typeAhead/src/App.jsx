1a12035785f3d6209c7b567e5b4e46a76c1691e2
clevercoderjoy <clevercoderjoy@gmail.com>
2024-10-03 00:02:13 +0530

    typehead Done

--- Day 11/typeAhead/src/App.jsx
+++ Day 11/typeAhead/src/App.jsx
@@ -1,92 +1,82 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { UPI_HANDLES } from './UPI_HANDLES';
 
 function App() {
 
-  const suggestions = UPI_HANDLES;
-
-
-  const [filteredSuggestions, setFilteredSuggestion] = useState(suggestions);
-  const [showSuggestions, setShowSuggestions] = useState(false);
-  const [activeIndex, setActiveIndex] = useState(0);
+  const suggestions = UPI_HANDLES.map((upi) => upi.toLowerCase());
   const [inputValue, setInputValue] = useState("");
+  const [filteredSuggestions, setFilteredSuggestions] = useState(suggestions);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const suggestionsRef = useRef(null);
 
   const handleInputChange = (e) => {
     const value = e.target.value;
-    if (value.split("@")[0].length > 0) {
-      setShowSuggestions(value.includes("@"));
-    }
     setInputValue(value);
-    handleSuggestions(value);
+    const [prefix, suffix] = value.split("@");
+    setShowSuggestions(prefix.length > 0 && value.includes("@"));
+    filteredSuggestionsList(suffix);
   }
 
-  const handleSuggestions = (value) => {
-    const [val, slug] = value.split("@");
-    const filteredUpis = suggestions.filter((upi) => upi.includes(slug));
-    setFilteredSuggestion(filteredUpis);
+  const filteredSuggestionsList = (suffix) => {
+    setFilteredSuggestions(suggestions.filter((upi) => upi.startsWith(suffix)));
     setActiveIndex(0);
   }
 
   const handleSelectedSuggestion = (suggestion) => {
-    const [beforeAt] = inputValue.split("@");
-    setInputValue(beforeAt + "@" + suggestion);
+    const [prefix] = inputValue.split("@");
+    setInputValue(prefix + "@" + suggestion);
     setShowSuggestions(false);
+    setActiveIndex(0);
   }
-
-  const handleKeyDown = (e) => {
-    if (inputValue.includes("@")) {
-      const [beforeAt] = inputValue.split("@");
-      if (e.key === "ArrowDown") {
-        setActiveIndex((prevIndex) => (prevIndex + 1) % filteredSuggestions.length);
-      }
-      if (e.key === "ArrowUp") {
-        setActiveIndex((prevIndex) => (prevIndex - 1 + filteredSuggestions.length) % filteredSuggestions.length);
-      }
-      if (e.key === "Enter") {
-        const selectedSuggestion = filteredSuggestions[activeIndex];
-        if (selectedSuggestion) {
-          handleSelectedSuggestion(selectedSuggestion);
-        }
-      }
-    }
-  };
 
   const handleMouseEnter = (index) => {
     setActiveIndex(index);
+    scrollToActive(index);
   }
+
+  const scrollToActive = (index) => {
+    const activeItem = suggestionsRef.current.children[index];
+    if (activeItem) {
+      activeItem.scrollIntoView({ block: "nearest", behavior: "smooth" })
+    }
+  }
+
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 38) {
+      e.preventDefault();
+      setActiveIndex(prevIndex => {
+        const newIndex = prevIndex === 0 ? filteredSuggestions.length - 1 : prevIndex - 1;
+        scrollToActive(newIndex);
+        return newIndex;
+      });
+    } else if (e.keyCode === 40) {
+      e.preventDefault();
+      setActiveIndex(prevIndex => {
+        const newIndex = prevIndex === filteredSuggestions.length - 1 ? 0 : prevIndex + 1;
+        scrollToActive(newIndex);
+        return newIndex;
+      });
+    } else if (e.keyCode === 13) {
+      if (filteredSuggestions[activeIndex]) {
+        handleSelectedSuggestion(filteredSuggestions[activeIndex]);
+      }
+    }
+  }
+
 
   return (
     <>
-      <div className="app-container">
-        <div className="input-container">
-          <input
-            type="text"
-            placeholder="Enter your UPI id..."
-            autoFocus
-            className="input-field"
-            onChange={handleInputChange}
-            onKeyDown={handleKeyDown}
-            value={inputValue}
-          />
-          <input
-            type="text"
-            disabled
-            className="input-field"
-          // value={inputValue}
-          />
+      <div className="inputContainer">
+        <input type="text" onChange={handleInputChange} value={inputValue} autoFocus onKeyDown={(e) => handleKeyDown(e)} />
+        <ul className="suggestionsContainer" style={{ display: showSuggestions ? "block" : "none" }} ref={suggestionsRef}>
           {
-            showSuggestions && (
-              <ul className={`suggestions-box`}>
-                {
-                  filteredSuggestions.map((suggestion, index) => <li className={`suggestion-item ${index === activeIndex ? "active" : ""}`} onClick={() => handleSelectedSuggestion(suggestion)} onMouseEnter={() => handleMouseEnter(index)} key={index}>{suggestion}</li>)
-                }
-              </ul>
-            )
+            filteredSuggestions.map((upi, index) => <li className={`upi ${index === activeIndex ? "active" : ""}`} key={index} onMouseEnter={() => handleMouseEnter(index)} onClick={() => handleSelectedSuggestion(upi)}>{upi}</li>)
           }
-        </div>
-      </div >
+        </ul>
+      </div>
     </>
   )
 }
 
-export default App
+export default App;
