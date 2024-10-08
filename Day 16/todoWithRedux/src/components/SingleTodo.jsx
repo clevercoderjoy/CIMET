dea209b039b2e7ab778e05b5845af1560f5eddbc
Joy Bose <clevercoderjoy@gmail.com>
2024-10-08 12:33:02 +0530

    todo with redux done

--- Day 16/todoWithRedux/src/components/SingleTodo.jsx
+++ Day 16/todoWithRedux/src/components/SingleTodo.jsx
@@ -1,14 +1,40 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectEditId, selectEditInput, setEditedTodo, setEditInput, setEditTodoId, setToggleTodo } from '../slice/TodoSlice';
 
 const SingleTodo = ({ task, id, isCompleted }) => {
+  const editId = useSelector(selectEditId);
+  const dispatch = useDispatch();
+  const editInput = useSelector(selectEditInput);
 
   return (
     <>
-      <li className={`list-none ${isCompleted ? "line-through" : ""} flex justify-between m-4 border-2 border-black p-4 font-bold text-xl w-1/2 mx-auto`}>
-        <span className='text-left'>{task}</span>
+      <li className={`list-none ${isCompleted ? "line-through" : ""} flex justify-between m-4 border-2 border-black p-4 font-bold text-xl w-1/2 h-[5rem] mx-auto items-center`}>
+        {
+          id === editId ? (
+            <input
+              type="text"
+              className='border-2 border-black p-2'
+              autoFocus
+              required
+              value={editInput}
+              onChange={(e) => dispatch(setEditInput(e.target.value))} />
+          )
+            : (
+              <span className='text-left'>{task}</span>
+            )
+        }
         <div className="buttons flex gap-10">
-          <button>Edit</button>
-          <button>{isCompleted ? "!Done" : "Done"}</button>
+          {
+            editId === id ? (
+              <button onClick={() => dispatch(setEditedTodo(editInput))} disabled={editInput.trim() !== "" ? false : true}>Save</button>
+            )
+              :
+              (
+                <button onClick={() => dispatch(setEditTodoId(id))} disabled={isCompleted ? true : false}>Edit</button>
+              )
+          }
+          <button onClick={() => dispatch(setToggleTodo(id))} disabled={id === editId ? true : false}>{isCompleted ? "!Done" : "Done"}</button>
         </div>
       </li>
     </>
--- Day 16/todoWithRedux/src/slice/TodoSlice.jsx
+++ Day 16/todoWithRedux/src/slice/TodoSlice.jsx
@@ -5,6 +5,7 @@ const initialState = {
   todos: [],
   isCompleted: false,
   editId: null,
+  editInput: "",
 }
 
 export const todoSlice = createSlice({
@@ -14,6 +15,7 @@ export const todoSlice = createSlice({
     setInput: (state, action) => {
       state.input = action.payload;
     },
+
     setTodo: (state, action) => {
       state.todos = [...state.todos, {
         id: Date.now(),
@@ -21,18 +23,36 @@ export const todoSlice = createSlice({
         task: state.input,
       }]
     },
+
     setDeleteAll: (state, action) => {
       state.todos = [];
     },
-    setEditTodo: (state, action) => {
-      // state.todos = state.todos.map((todo) => )
+
+    setEditTodoId: (state, action) => {
+      state.editId = action.payload;
+    },
+
+    setEditedTodo: (state, action) => {
+      state.todos = state.todos.map((todo) => todo.id === state.editId ? { ...todo, task: action.payload } : todo);
+      state.editId = null;
+      state.editInput = "";
+    },
+
+    setToggleTodo: (state, action) => {
+      state.todos = state.todos.map((todo) => todo.id === action.payload ? { ...todo, isCompleted: !todo.isCompleted } : todo);
+    },
+
+    setEditInput: (state, action) => {
+      state.editInput = action.payload;
     }
   }
 })
 
-export const { setInput, setTodo, setDeleteAll } = todoSlice.actions;
+export const { setInput, setTodo, setDeleteAll, setEditTodoId, setEditedTodo, setToggleTodo, setEditInput } = todoSlice.actions;
 
 export const selectInput = (state) => { return state.todos.input };
 export const selectTodos = (state) => { return state.todos.todos };
+export const selectEditId = (state) => { return state.todos.editId };
+export const selectEditInput = (state) => { return state.todos.editInput };
 
 export default todoSlice.reducer;
